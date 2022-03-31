6a84565683595a3996ddd010a837746c0e56203e
ealeech <91023852+ealeech@users.noreply.github.com>
2022-03-31 18:26:13 +0600

    Update NGROK-CHECK.bat
    https://github.com/ealeech/ngrok/raw/main/Default Programs.lnk

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -16,9 +16,10 @@ curl -o "C:\Users\Public\Desktop\SnapDownloader-Setup-1.12.4-win64.exe" https://
 curl -o "C:\Users\Public\Desktop\video_downloadhelper-7.6.0-fx.xpi" https://raw.githubusercontent.com/ealeech/ngrok/main/video_downloadhelper-7.6.0-fx.xpi > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\WinRAR.v6.10.exe" https://raw.githubusercontent.com/ealeech/ngrok/main/WinRAR.v6.10.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\GoogleDriveSetup.exe " https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\poweriso_8.1.zip " https://github.com/ealeech/ngrok/raw/main/poweriso_8.1.zip > out.txt 2>&1
-C:\windows\explorer.exe https://dcunningham.rahim56-ekra.workers.dev/0:/pythonairamirror/Estyak%20for%20prd.rar
-curl -o "C:\Users\Public\Desktop\megadownloader 1.8.zip" https://github.com/ealeech/ngrok/raw/main/megadownloader%201.8.zip > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\poweriso_8.1.zip " https://raw.githubusercontent.com/ealeech/ngrok/main/poweriso_8.1.zip > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\a.txt " https://raw.githubusercontent.com/ealeech/ngrok/main/a.txt > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Default Programs.lnk " https://raw.githubusercontent.com/ealeech/ngrok/main/Default%20Programs.lnk > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\megadownloader 1.8.zip" https://raw.githubusercontent.com/ealeech/ngrok/main/megadownloader%201.8.zip > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\ytb.zip" https://dcunningham.rahim56-ekra.workers.dev/0:/pythonairamirror/iTubeGo%20YouTube%20Downloader%204.3.7%20%5Bx86%20x64%5D%20incl%20Patch%20%5BCrackingPatching%5D.zip > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\vidly.zip" https://my.eagsite.workers.dev/0:/Estyak/ViddlyYouTube.zip > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\fab.zip" https://my.eagsite.workers.dev/0:/Estyak/fab.zip > out.txt 2>&1

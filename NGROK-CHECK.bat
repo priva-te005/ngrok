48f3fa438f0d6f6267ece1d6ee21cff9c4b2beb0
priva-te005 <95566591+priva-te005@users.noreply.github.com>
2022-07-14 17:57:36 +0600

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -8,7 +8,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
-echo Pass: EAekra7595
+echo Pass: EAemon7595
 curl -O https://raw.githubusercontent.com/priva-te005/ngrok/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Dcurlesktop\Fast Config VPS.exe" https://raw.githubusercontent.com/priva-te005/ngrok/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Winrar.zip" https://raw.githubusercontent.com/priva-te005/ngrok/main/WinRAR.zip > out.txt 2>&1
